b06310efffba4754074e054085225589fa91fbe0
santatra <razafindratsimba34@gmail.com>
2023-09-06 12:29:12 +0300

    made Header responsive and fix Breadcrumb clustered design

--- src/Components.js
+++ src/Components.js
@@ -7,6 +7,11 @@ function Header(){
     return(
         <div id="header" className="dark-banner">
             <img id="brand" src="img/logo.png" alt="logo" />
+            <svg id="burger-menu" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M4 18L20 18" stroke="#fff" stroke-width="2" stroke-linecap="round"/>
+                <path d="M4 12L20 12" stroke="#fff" stroke-width="2" stroke-linecap="round"/>
+                <path d="M4 6L20 6" stroke="#fff" stroke-width="2" stroke-linecap="round"/>
+            </svg>
             <div id="menu">
                 <div> Cocktails </div>
                 <div> Tips & Tricks </div>
