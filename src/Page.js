7437df1720d1d4bb5db5a18854ca3c1b66c9a88e
santatra <razafindratsimba34@gmail.com>
2023-09-06 15:33:08 +0300

    add toggleModal for mobile

--- src/Page.js
+++ src/Page.js
@@ -1,7 +1,16 @@
 import * as React from 'react';
+import {useEffect} from 'react';
 import { Header, Breadcrumb, CardList, Newsletter, Footer} from './Components.js';
 
 function Home() {
+        // Import script modal.js
+        useEffect(() => {
+                const body = document.querySelector('body');
+                const script = document.createElement('script');
+                script.setAttribute('src', 'modal.js');
+                body.appendChild(script);
+        }, []);
+
         return (
         <>
                 <Header/>
