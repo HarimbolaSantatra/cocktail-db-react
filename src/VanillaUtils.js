2de2bfc042544f680c20a4ae83d53f3e846b512b
santatra <razafindratsimba34@gmail.com>
2023-09-08 18:55:19 +0300

    add VanillaUtils

---
+++ src/VanillaUtils.js
@@ -0,0 +1,13 @@
+/*
+ * Get a random integer.
+ The maximum is exclusive and the minimum is inclusive.
+*/
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+
+
+export { getRandomInt };
